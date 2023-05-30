174e6f49ffa44e19f8774c27523b47f27756adcc
Dinh Nguyen <nguyen_dinh282@yahoo.com>
2023-05-29 23:25:39 -0400

    add code for GET Route and POST Route to notes.js

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,34 +1,59 @@
 import Router from 'express';
-import writeFile from 'fs';
-
+import fs from 'fs';
+import util from 'util';
 
 
 const notesRouter = Router();
-
+const readFromFile = util.promisify(fs.readFile);
+const writeToFile = util.promisify(fs.writeFile);
 
 
 // GET Route for retrieving all notes
-notesRouter.get('/', (req, res) =>
+notesRouter.get('/notes', (req, res) =>
     readFromFile('./db/db.json')
     .then((data) => res.json(JSON.parse(data)))
     .catch((err) => {
         console.error('Error reading data from db.json', err);
-        res.status(500)
+        res
+        .status(500)
         .json({error: 'Failed to retrieve data from the database'});
     })
 );
 
 
 // POST Route for saving note entries.
-notesRouter.post('/', (req, res) => {
+notesRouter.post('/notes', (req, res) => {
   
-    const newEntry = {
-        title: req.body.title,
-        text: req.body.text,
-    }
-    
+const { title, text } = req.body;
 
-    
+
+if(title && text) {
+
+    const newEntry = {
+        title,
+        text,
+    }
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+          if (err) {
+            console.error(err);
+          } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(newEntry);
+            writeToFile('./db/db.json', JSON.stringify(parsedData));
+          }
+        });
+     
+    const response = {
+        status: 'success',
+        body: newEntry,
+    };
+
+    res.json(response);
+    }else{
+        res
+        .status(500)
+        .json('Error in posting new entry.')
+    }
     
     
 });
